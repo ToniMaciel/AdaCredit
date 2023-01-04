f86f3bb27e1d99959ac8af40a6c7f895fbfd2bfb
ToniMaciel <jaam@cin.ufpe.br>
2023-01-03 22:15:10 -0300

    Kickstart

---
+++ AdaCreditApplication.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AdaCredit
+{
+    class AdaCreditApplication
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AdaCredit!");
+        }
+    }
+}
